f4d822a354c689cc65178de32cf455e3f835123b
fkymon <mbtbrk11@outlook.com>
2022-01-29 19:39:24 +0300

    BTK 6.1

---
+++ Conditionals/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditionals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 95;
+
+            if (number < 100)
+            {
+                if(number > 90)
+                {
+                    Console.WriteLine(number);
+                }
+            }           
+
+            Console.WriteLine("#Kod çalıştı#");
+            Console.ReadLine();
+        }
+    }
+}
