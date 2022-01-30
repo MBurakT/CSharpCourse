1be407b6b30c70ff70e6da2ff369b4a3f1d5eca3
fkymon <mbtbrk11@outlook.com>
2022-01-30 15:53:27 +0300

    BTK 8.5

---
+++ AsalSayi/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsalSayi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+            // Do Not Auto Close, Write Code Executed İnfo, Enter Something To Close
+            Console.WriteLine("#Kod çalıştı#");
+            Console.ReadLine();
+
+        }
+
+        private static bool asal;
+
+    }
+}
---
+++ Loops/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // foreach readonly durumdadır
+
+            string[,] regions = new string[5, 3]
+            {
+                { "İstanbul", "Bilecik", "Balıkesir" },
+                { "Ankara", "Konya", "Kırıkkale" },
+                { "Antalya", "Adana", "Mersin" },
+                { "Rize", "Trabzon", "Samsun" },
+                { "İzmir", "Muğla", "Aydın" }
+            };
+            int i = regions.GetUpperBound(1);
+
+            //ForLoop();
+            //WhileLoop();
+            //DoWhileLoop();
+            foreach (var region in regions)
+            {
+                if (i == 0)
+                {
+                    Console.WriteLine(region);
+                    i = regions.GetUpperBound(1);
+                }
+                else
+                {
+                    Console.Write(region + " (\\ô.ô/) ");
+                    i--;
+                }
+            }
+
+            // Do Not Auto Close, Write Code Executed İnfo, Enter Something To Close
+            Console.WriteLine("#Kod çalıştı#");
+            Console.ReadLine();
+
+        }
+
+        private static void ForLoop()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                if (i / 2 * 2 == i)
+                {
+                    Console.Write(i + " (\\ô.ô/) ");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        private static void WhileLoop()
+        {
+            int i = 0;
+            while (i < 100)
+            {
+                if (i / 2 * 2 == i)
+                {
+                    Console.Write(i + " (\\ô.ô/) ");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+                i++;
+            }
+            Console.WriteLine(i);
+        }
+
+        private static void DoWhileLoop()
+        {
+            int i = 0;
+            do
+            {
+                if (i / 2 * 2 == i)
+                {
+                    Console.Write(i + " (\\ô.ô/) ");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+                i++;
+            } while (i < 100);
+            Console.WriteLine(i);
+        }
+    }
+}
