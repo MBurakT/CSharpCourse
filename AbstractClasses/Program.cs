6bf2d96cfcdff7ed22551eb123683cd485b14277
fkymon <mbtbrk11@outlook.com>
2022-02-05 15:22:09 +0300

    BTK 15.1

---
+++ AbstractClasses/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Database database = new Oracle();
+            Database database2 = new SqlServer();
+
+            database.Add();
+            database.Delete();
+            database2.Add();
+            database2.Delete();
+
+            // Do Not Auto Close, Write Code Executed İnfo, Enter Something To Close
+            Console.WriteLine("#Kod çalıştı#");
+            Console.ReadLine();
+        }
+    }
+
+    //Abstract classlar da interfaceler gibi 'new' yapılamaz
+    //sadece 1 tane abstract class implement edilebilir
+    abstract class Database
+    {
+        public void Add()
+        {
+            Console.WriteLine("Added by default");
+        }
+        public abstract void Delete();
+    }
+
+    class SqlServer : Database
+    {
+        public override void Delete()
+        {
+            Console.WriteLine("Deleted by SqlServer");
+        }
+    }
+
+    class Oracle : Database
+    {
+        public override void Delete()
+        {
+            Console.WriteLine("Deleted by Oracle");
+        }
+    }
+}
