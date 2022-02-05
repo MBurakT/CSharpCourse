da380042aeedfb4c4876d7b8b912a0a32ccc8919
fkymon <mbtbrk11@outlook.com>
2022-02-05 15:08:08 +0300

    BTK 14.1

---
+++ Inheritance/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person[3]
+            {
+                new Customer{FirstName="Burak"},
+                new Student{FirstName="Fobon"},
+                new Person{FirstName="Fkymon"}
+            };
+
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person.FirstName);
+            }
+
+            // Do Not Auto Close, Write Code Executed İnfo, Enter Something To Close
+            Console.WriteLine("#Kod çalıştı#");
+            Console.ReadLine();
+        }
+    }
+
+    //Herhangi bir sınıfın sadece 1 tane inheritance'ı olabilirken birden fazla interface'i olabilir
+    //Interfacelerden 'new' ile örnek oluşturmak bir anlam ifade etmezken dizi olarak içinde implemente edilmiş olduğu classları tutabilir
+    //Implement yapılırken önce inheritance varsa interfaceler yazılır
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    class Customer : Person
+    {
+        public string City { get; set; }
+    }
+
+    class Student : Person
+    {
+        public string Department { get; set; }
+    }
+}
---
+++ VirtualMethods/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlServer sqlServer = new SqlServer();
+            MySql mySql = new MySql();
+
+            sqlServer.Add();
+            mySql.Add();
+
+            // Do Not Auto Close, Write Code Executed İnfo, Enter Something To Close
+            Console.WriteLine("#Kod çalıştı#");
+            Console.ReadLine();
+        }
+    }
+
+    class Database
+    {
+        //override etmek istediğimiz methodlara 'virtual' ekleriz
+        public virtual void Add()
+        {
+            Console.WriteLine("Added by default");
+        }
+
+        public virtual void Delete()
+        {
+            Console.WriteLine("Deleted by default");
+        }
+    }
+
+    class SqlServer:Database
+    {
+        //base üst sınıfı temsil eder
+        public override void Add()
+        {
+            Console.WriteLine("Added by SqlServer");
+            //base.Add();
+        }
+    }
+
+    class MySql : Database
+    {
+        public override void Add()
+        {
+            Console.WriteLine("Added by MySql");
+            base.Add();
+        }
+    }
+}
