dec3c305bd5654d41a9d275b397db665b96bb2ef
fkymon <mbtbrk11@outlook.com>
2022-02-06 13:45:06 +0300

    BTK 17.1

---
+++ AccessModifiers/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // Do Not Auto Close, Write Code Executed İnfo, Enter Something To Close
+            Console.WriteLine("#Kod çalıştı#");
+            Console.ReadLine();
+        }
+    }
+
+    //private sadece tanımlandığı scope içinde kullabilirken protected inheritance edildiği sınıflarda da kullanılabilir
+    //private değişkenler için default erişim bildirgeçidir
+    class Customer
+    {
+        private int id;
+        protected int id2;
+    }
+
+    class Student : Customer
+    {
+        public void Save()
+        {
+            Customer customer = new Customer();
+        }
+    }
+    //internal bağlı bulunduğu projede referans ihtiyacı olmadan kullanılabilir, default class erişim bildirgeçidir
+    //bir class private veya protected olarak tanımlanamaz, ya internal ya da public olarak tanımlanır
+    //iç içe classlarda private olabilir
+    //farklı bir projeden de kullanmak istediğimiz classları public olarak tanımlayabiliriz
+    public class Course
+    {
+        public int Name { get; set; }
+
+        private class NestedClass
+        {
+             
+        }
+    }
+}
---
+++ AccessModifiersDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccessModifiers;
+
+namespace AccessModifiersDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Başka bir projeden bir classı kullanmak için önce referans olarak eklememiz ve kullanacağımız yere using yaparak eklememiz gerekir
+            //Ayrıca başka bir projeden kullanmak istediğimiz classlar public olarak tanımlanmalıdır
+            Course course = new Course();
+
+            // Do Not Auto Close, Write Code Executed İnfo, Enter Something To Close
+            Console.WriteLine("#Kod çalıştı#");
+            Console.ReadLine();
+        }
+    }
+}
