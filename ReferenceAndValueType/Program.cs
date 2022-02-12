4cffaebc301a74cec4bd40382f2278a1c74ba153
fkymon <mbtbrk11@outlook.com>
2022-02-12 10:36:44 +0300

    BTK 19.1

--- ReferenceAndValueType/Program.cs
+++ ReferenceAndValueType/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,24 @@ namespace ReferenceAndValueType
     {
         static void Main(string[] args)
         {
+            int number1 = 10;
+            int number2 = 20;
+            number2 = number1;
+            number1 = 30;
 
-            
+            Console.WriteLine(number2);
+
+            //Arrayler referans tiptedir. Bellekte Array için bir referans ve arrayin değerlerini için başka bir referans tutulur
+            string[] cities1 = new string[] {"Ankara", "Adana", "Afyon"};
+            string[] cities2 = new string[] { "Bilecik", "Balıkesir", "Bursa" };
+            cities2 = cities1;
+            cities1[0] = "İstanbul";
+            Console.WriteLine(cities2[0]);
+
+            DataTable dataTable;
+            DataTable dataTable2 = new DataTable();
+            dataTable = dataTable2;
+
             // Do Not Auto Close, Write Code Executed İnfo, Enter Something To Close
             Console.WriteLine("#Kod çalıştı#");
             Console.ReadLine();
