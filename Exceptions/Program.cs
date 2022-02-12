e6729f80ee2222dbe85e9fc1e99e9c9681b54c2b
fkymon <mbtbrk11@outlook.com>
2022-02-12 16:22:54 +0300

    BTK 21.2

--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -10,11 +10,66 @@ namespace Exceptions
     {
         static void Main(string[] args)
         {
+            //ExceptionIntro();
 
+            HandleException(() => {
+                Find();
+            });
 
             // Do Not Auto Close, Write Code Executed İnfo, Enter Something To Close
             Console.WriteLine("#Kod çalıştı#");
             Console.ReadLine();
+        }
+
+        private static void HandleException(Action action)
+        {
+            try
+            {
+                action.Invoke();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+
+        private static void Find()
+        {
+            List<string> students = new List<string> { "Burak", "Fobon", "Fkymon" };
+
+            if (!students.Contains("Ahmet"))
+            {
+                throw new RecordNotFoundException("Record not found!");
+            }
+            else
+            {
+                Console.WriteLine("Record Found");
+            }
+        }
+
+        private static void ExceptionIntro()
+        {
+            try
+            {
+                string[] students = new string[3]
+                {
+                "Burak", "Fobon", "Fkymon"
+                };
+
+                students[3] = "Ahmet";
+            }
+            catch (IndexOutOfRangeException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            catch (DivideByZeroException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
         }
     }
 }
