d28ac4f00aa2fac227b2b1acdc2dfa3993cf94c3
fkymon <mbtbrk11@outlook.com>
2022-02-20 16:35:42 +0300

    BTK 25.1

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -10,7 +10,139 @@ namespace Generics
     {
         static void Main(string[] args)
         {
+            Utilities utilities = new Utilities();
 
+            List<string> result = utilities.BuildList<string>("Ankara", "İzmir", "Adana");
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+
+            List<Customer> result2 = utilities.BuildList<Customer>(new Customer { FirstName = "Burak" }, new Customer { FirstName = "Fkymon" });
+            foreach (var customer in result2)
+            {
+                Console.WriteLine(customer.FirstName);
+            }
+
+            // Do Not Auto Close, Write Code Executed İnfo, Enter Something To Close
+            Console.WriteLine("#Kod çalıştı#");
+            Console.ReadLine();
+        }
+    }
+
+    class Utilities
+    {
+        public List<T> BuildList<T>(params T[] items)
+        {
+            return new List<T>(items);
+        }
+    }
+
+    class Product:IEntity
+    {
+
+    }
+
+    interface IProductDal : IRepository<Product>
+    {
+
+    }
+
+    class Customer:IEntity
+    {
+        public string FirstName { get; set; }
+    }
+
+    interface ICustomerDal : IRepository<Customer>
+    {
+        void Custom();
+    }
+
+    interface IStudentDal : IRepository<Student>
+    {
+
+    }
+
+    class Student : IEntity
+    {
+
+    }
+
+    interface IEntity
+    {
+
+    }
+
+    //referans tip(class), veritabanı nesnesi(IEntity'den implemente edilmiş) ve new yapılabilir olmalı
+    //new() her zaman en sonda olmalı
+    //değer tipler için where T : struct 
+    interface IRepository<T> where T : class, IEntity, new()
+    {
+        List<T> GetAll();
+        T Get(int id);
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+    }
+
+    class ProductDal : IProductDal
+    {
+        public void Add(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class CustomerDal : ICustomerDal
+    {
+        public void Add(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Custom()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Customer> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Customer entity)
+        {
+            throw new NotImplementedException();
         }
     }
 }
