e35bf10ace80a0623d34f03db3f1661f9950e0ff
fkymon <mbtbrk11@outlook.com>
2022-02-26 12:06:43 +0300

    BTK 26.1

--- Attributes/Program.cs
+++ Attributes/Program.cs
@@ -10,11 +10,65 @@ namespace Attributes
     {
         static void Main(string[] args)
         {
+            Customer customer = new Customer
+            {
+                Id = 1,
+                Age = 25,
+                //FirstName = "Burak",
+                LastName = "Turhan"
+            };
+            CustomerDal customerDal = new CustomerDal();
+            customerDal.Add(customer);
 
 
             // Do Not Auto Close, Write Code Executed İnfo, Enter Something To Close
             Console.WriteLine("#Kod çalıştı#");
             Console.ReadLine();
+        }
+    }
+
+    //Attribute nesnelere özellik vermek ve merkezi bir noktada kontrol yapmak için kullanılır
+    [ToTable("Customers")]
+    [ToTable("TblCustomers")]
+    class Customer
+    {
+        public int Id { get; set; }
+        [RequiredProperty]
+        public int Age { get; set; }
+        [RequiredProperty]
+        public string FirstName { get; set; }
+        [RequiredProperty]
+        public string LastName { get; set; }
+    }
+
+    class CustomerDal
+    {
+        [Obsolete("Don't use Add, instead use AddNew Method")]
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("{0}, {1}, {2}, {3} added!", customer.Id, customer.Age, customer.FirstName, customer.LastName);
+        }
+
+        public void AddNew(Customer customer)
+        {
+            Console.WriteLine("{0}, {1}, {2}, {3} added!", customer.Id, customer.Age, customer.FirstName, customer.LastName);
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
+    class RequiredPropertyAttribute : Attribute
+    {
+
+    }
+
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    class ToTableAttribute : Attribute
+    {
+        string _tablename;
+
+        public ToTableAttribute(string tablename)
+        {
+            _tablename = tablename;
         }
     }
 }
