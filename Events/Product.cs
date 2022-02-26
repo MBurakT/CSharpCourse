1522c7ee095632e8a7dc80c91920fe232596e448
fkymon <mbtbrk11@outlook.com>
2022-02-26 15:48:25 +0300

    BTK 29.1

---
+++ Events/Product.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    //Event'ler aslında bir delegate'tir, delegate'lerin kullanımıyla yapabileceğimiz bir işlemdir
+    public delegate void StockControl();
+    public class Product
+    {
+        //Normalde veritabanından çekilir
+        private int _stock;
+
+        public Product(int stock)
+        {
+            _stock = stock;
+        }
+
+        public event StockControl StockControlEvent;
+        public string ProductName { get; set; }
+
+        public int Stock
+        {
+            get
+            {
+                return _stock;
+            }
+            set
+            {
+                _stock = value;
+                if (value <= 15 && StockControlEvent != null)
+                {
+                    StockControlEvent();
+                }
+            }
+        }
+
+        public void Sell(int amount)
+        {
+            Stock -= amount;
+            Console.WriteLine("{1} stock amount : {0}", Stock, ProductName);
+        }
+    }
+}
--- Events/Program.cs
+++ Events/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Events
 {
@@ -10,11 +11,32 @@ namespace Events
     {
         static void Main(string[] args)
         {
+            Product harddisk = new Product(50);
+            harddisk.ProductName = "Hard Disk";
 
+            Product gsm = new Product(50);
+            gsm.ProductName = "GSM";
+            gsm.StockControlEvent += Gsm_StockControlEvent;
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (harddisk.Stock < 1 || gsm.Stock < 1)
+                {
+                    break;
+                }
+                harddisk.Sell(10);
+                gsm.Sell(10);
+                Console.ReadLine();
+            }
 
             // Do Not Auto Close, Write Code Executed İnfo, Enter Something To Close
             Console.WriteLine("#Kod çalıştı#");
             Console.ReadLine();
+        }
+
+        private static void Gsm_StockControlEvent()
+        {
+            MessageBox.Show("Gsm about to finish!!!");
         }
     }
 }
